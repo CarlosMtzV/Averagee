30deafa540eb9afe5bae56f9555af5c17d00e533
CarlosMtzV <159225079+CarlosMtzV@users.noreply.github.com>
2024-02-26 23:57:05 -0600

    average

---
+++ Average/Program.cs
@@ -0,0 +1,10 @@
+﻿
+using Average;
+int[] integerData = { 8, 8, 9, 9, 8, 6 };
+
+double[] floatingData = { 8.8, 8.8, 9.9, 9.9, 8.8, 6.6 };
+
+double integerAverage = Statistics.CalculateAverage(integerData);
+double floatingAverage = Statistics.CalculateAverage(floatingData);
+Console.WriteLine("Average of integer numbers: " + integerAverage);
+Console.WriteLine("Average of floating-point numbers: " + floatingAverage);
---
+++ Average/Statistics.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Average
+{
+    internal class Statistics
+    {
+        public static double CalculateAverage(int[] data)
+        {
+            double sum = data.Sum();
+            return sum / data.Length;
+        }
+
+        public static double CalculateAverage(double[] data)
+        {
+            double sum = data.Sum();
+            return sum / data.Length;
+        }
+    }
+}
